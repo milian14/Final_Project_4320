55cd97b1b05c88cfdbb7cc0bef0e8df43d5ab454
Blake Miller <blamiller58@gmail.com>
2023-12-04 22:27:06 -0600

    Added reservations.html and modified other html files to facilitate changes

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def submit():
     if selected_option == 'option2':
         # Redirect or handle Admin Login
         return redirect('/admin_login')
+    elif selected_option == 'option3':
+        # Redirect or handle Reservation
+        return redirect('/reserve_a_seat')
     else:
         # Handle default or invalid option
         return redirect('/')
@@ -25,6 +28,11 @@ def submit():
 def admin_login():
     # Render the admin login template or handle logic
     return render_template('adminLogin.html')
+
+@app.route('/reserve_a_seat')
+def reserve_a_seat():
+    # Render the admin login template or handle logic
+    return render_template('reservations.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
